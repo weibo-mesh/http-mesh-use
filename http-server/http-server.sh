b8f77617f25729602f1232e8e945b9c865f5d5a3
idevz <zhoujing_k49@163.com>
2018-12-25 20:55:30 +0800

    update http server script

--- http-server/http-server.sh
+++ http-server/http-server.sh
@@ -13,41 +13,4 @@
 set -e
 BASE_DIR=${BASE_DIR:-"$(readlink -f "$(dirname "$0")")"}
 
-s() {
-	while getopts "xs" OPTS; do
-		case "${OPTS}" in
-		x)
-			fg
-			;;
-		s)
-			python3 -m http.server --directory "${BASE_DIR}/html/" &
-			;;
-		\?)
-			echo "
-k8 is a tool for K8S.
-
-proxy="http://10.211.55.5:8080"
-export ALL_PROXY=${proxy}
-export HTTP_PROXY=${proxy}
-export HTTPS_PROXY=${proxy}
-export all_proxy=${proxy}
-export http_proxy=${proxy}
-export https_proxy=${proxy}
-
- $ALL_PROXY $HTTP_PROXY $HTTPS_PROXY $all_proxy $http_proxy $https_proxy
-Usage:
-
-	k8 options [arguments]
-
-The options are:
-
-	-u       start the php-fpm and openresty service.
-	         php-fpm run path is ${RUN_PATH}/fpm.d
-			 php ini path is ${RUN_PATH}/fpm.d/etc
-			 openresty run path is ${RUN_PATH}/ngx.d
-	-d       down both openresty and php-fpm service.
-		"
-			;;
-		esac
-	done
-}
+python3 -m http.server --directory "${BASE_DIR}/html/" &
